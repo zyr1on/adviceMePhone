e36bf6cccdd2ecdfe02e9982e1806459f2d54596
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-05-31 16:18:04 +0300

    Update install.py

--- install.py
+++ install.py
@@ -15,13 +15,17 @@ else:
 
 try:
     subprocess.check_call(pip_command)
-    print("Bağımlılıklar başarıyla yüklendi.")
+    print("\nBağımlılıklar başarıyla yüklendi.\n")
 except subprocess.CalledProcessError as e:
     print(f"Bağımlılıkları yüklerken bir hata oluştu: {e}")
 
-print("Model dosyası yükleniyor....")
-import gdown
-url = 'https://drive.google.com/uc?export=download&id=1vtK37taMctc1mMG84Mx1OHN0DItJY3hW'
-output= 'model.pt'
-gdown.download(url, output, quiet=False, proxy=False)
-print("Model dosyası yüklendi")
+
+if not os.path.exists("model.pt"):
+    import gdown
+    url = 'https://drive.google.com/uc?export=download&id=1vtK37taMctc1mMG84Mx1OHN0DItJY3hW'
+    output= 'model.pt'
+    gdown.download(url, output, quiet=False, proxy=False)
+    print("Model dosyası yüklendi.")
+    print("python3 app.py ile programı çalıştırabilirsiniz.")
+else:
+    print("Model dosyası zaten mevcut\n 'python3 app.py' ile programı çalıştırabilirsiniz.")
