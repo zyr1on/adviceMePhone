19825cdc1136e64af3beb126d307522005d02660
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-05-31 13:21:35 +0300

    Update install.py

--- install.py
+++ install.py
@@ -1,3 +1,26 @@
-def installPTModel():
-	pass
+import os
+import platform
+import subprocess
+import gdown
 
+system_platform = platform.system()
+requirements_file = 'requirements.txt'
+
+if system_platform == 'Windows':
+    pip_command = ['pip.exe', 'install', '-r', requirements_file]
+elif system_platform == 'Linux' or system_platform == 'Darwin':  # MacOS 'Darwin' olarak tanımlanır
+    pip_command = ['pip3', 'install', '-r', requirements_file]
+else:
+    raise EnvironmentError(f"Desteklenmeyen işletim sistemi: {system_platform}")
+
+try:
+    subprocess.check_call(pip_command)
+    print("Bağımlılıklar başarıyla yüklendi.")
+except subprocess.CalledProcessError as e:
+    print(f"Bağımlılıkları yüklerken bir hata oluştu: {e}")
+
+print("Model dosyası yükleniyor....")
+url = ‘https://drive.google.com/uc?export=download&id=1vtK37taMctc1mMG84Mx1OHN0DItJY3hW'
+output= ‘model.pt’
+gdown.download(url, output, quiet=False, proxy=False)
+print("Model dosyası yüklendi")
